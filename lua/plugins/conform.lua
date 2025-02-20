6ee5339e927755568343dda5d479b50187091f79
legi0n <me@alexandrujoita.com>
2025-02-20 20:55:48 +0200

    modified:   lua/plugins/conform.lua

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -18,23 +18,17 @@ return {
         notify_on_error = false, -- Do not notify on error
         format_on_save = function(bufnr)
             -- Disable "format_on_save lsp_fallback" for languages that don't
-            -- have a well-standardized coding style. You can add additional
+            -- have a well standardized coding style. You can add additional
             -- languages here or re-enable it for the disabled ones.
-
-            local disable_filetypes = { c = true, cpp = true } -- Disable for C/C++ files
-            local lsp_format_opt
-
-            -- Choose formatter based on filetype
+            local disable_filetypes = { c = true, cpp = true }
             if disable_filetypes[vim.bo[bufnr].filetype] then
-                lsp_format_opt = 'never' -- Disable LSP formatting for disabled filetypes
+                return
             else
-                lsp_format_opt = 'fallback' -- Use fallback formatting for others
+                return {
+                    timeout_ms = 500,
+                    lsp_format = 'fallback',
+                }
             end
-
-            return {
-                timeout_ms = 500, -- Timeout for formatting operation
-                lsp_format = lsp_format_opt, -- LSP fallback or never based on filetype
-            }
         end,
         formatters_by_ft = {
             -- Formatter settings by filetype
@@ -47,17 +41,10 @@ return {
         },
         formatters = {
             ['clang-format'] = {
-                prepend_args = function(ctx)
-                    local config_file = vim.fs.find({ '.clang-format' }, { path = ctx.filename, upward = true })
-                    if config_file[1] then
-                        return {}
-                    else
-                        return {
-                            '--style',
-                            '{IndentWidth: 4, TabWidth: 4, UseTab: Never}',
-                        }
-                    end
-                end,
+                prepend_args = {
+                    '--style',
+                    '{IndentWidth: 4, TabWidth: 4, UseTab: Never}',
+                },
             },
         },
     },
